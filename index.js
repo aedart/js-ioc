b183da7fe3309c71d45c60b8429e016e300f2de5
Alin Eugen Deac <aedart@gmail.com>
2016-10-29 15:48:51 +0200

    Add index

---
+++ index.js
@@ -0,0 +1,18 @@
+
+// Entries
+import Binding from './src/Entries/Binding';
+export {Binding};
+
+// Exceptions
+import IoCError from './src/Exceptions/IoCError';
+export {IoCError};
+
+import BindingException from './src/Exceptions/BindingException';
+export {BindingException};
+
+import BuildException from './src/Exceptions/BuildException';
+export {BuildException};
+
+// Container
+import Container from './src/Container';
+export default Container;
